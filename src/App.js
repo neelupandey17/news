d65809a4f64e855c1a4171d796dfb838c85846b7
neelupandey17 <102846813+neelupandey17@users.noreply.github.com>
2022-04-02 12:52:07 +0530

    Add files via upload
    add in m

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import News from "./components/News";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Newsletter evidence</h1>
+      </header>
+      <News />
+    </div>
+  );
+}
+
+export default App;
